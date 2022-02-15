641aedfb6d3f91492968a45de4737d3dffb01b34
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2022-02-15 18:51:00 +0530

    main things are completed

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,8 @@
 import React from 'react'
 
-const Form = ({ setInputText, todos, setTodos, inputText }) => {
+const Form = ({ setInputText, todos, setTodos, inputText, setStatus }) => {
     //here i can write js code
     const inputTextHandler = (e) => {
-        console.log(e.target.value)
         setInputText(e.target.value)
     }
     const submitTodoHandler = (e) => {
@@ -14,6 +13,9 @@ const Form = ({ setInputText, todos, setTodos, inputText }) => {
         ])
         setInputText("");
     }
+    const statusHandler = (e) => {
+        setStatus(e.target.value)
+    }
 
     return (
         <form>
@@ -22,7 +24,7 @@ const Form = ({ setInputText, todos, setTodos, inputText }) => {
                 <i className="fas fa-plus-square"></i>
             </button>
             <div className="select">
-                <select name="todos" className="filter-todo">
+                <select name="todos" className="filter-todo" onChange={statusHandler}>
                     <option value="all">All</option>
                     <option value="completed">Completed</option>
                     <option value="uncompleted">Uncompleted</option>

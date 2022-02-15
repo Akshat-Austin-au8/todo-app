3378362132bebdfd657662190776dc2264333e82
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2022-02-15 12:53:30 +0530

    file structure added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Akshat's Todo List</h1>
       </header>
     </div>
   );
